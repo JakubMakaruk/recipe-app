240883824644e12ea8ad5538d7d2ca4cb768b354
JakubMakaruk <jakub.makaruk@gmail.com>
2024-11-08 19:31:18 +0100

    update angular to version 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {PreloadModulesStrategy} from './preload-modules.strategy';
-import {SharedModule} from './shared/shared.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PreloadModulesStrategy } from './preload-modules.strategy';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,10 +14,9 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HttpClientModule,
     BrowserAnimationsModule,
   ],
-  providers: [PreloadModulesStrategy],
+  providers: [provideHttpClient(), PreloadModulesStrategy],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
